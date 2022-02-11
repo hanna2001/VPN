e21ff8756d1181ff346c6962deadbf86445b82a4
rameesrms <rameesrms000@gmail.com>
2022-02-11 18:08:38 +0530

    first page

---
+++ lib/constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+Color bgOne = Color(0xff002e99);
+Color bgTwo = Color(0xff6699cc);
+Color btColor = Color(0xff0066cc);
+
+TextStyle styleOne= TextStyle(
+    fontSize: 26,fontWeight: FontWeight.bold,color: Colors.white);
+
+TextStyle styleTwo= TextStyle(fontWeight: FontWeight.normal,
+    fontSize: 15,color: Colors.white70);
